b2f7286f94aa003663eefdfa5d20c190348f0de0
NayeraWael <nairawael1412@gmail.com>
2022-12-19 18:48:39 -0800

    swap

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "kasiye.h"
+
+/**
+ * reser_to_98n - take a pointer to an int as parameter and updates
+ * the value it points to 98
+ * @n: point to an int
+ * return: Nothing
+ */
+void reset_to_98(int *n);
+{
+	*n = 98;
+}	
